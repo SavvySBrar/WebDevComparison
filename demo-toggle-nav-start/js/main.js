48d2062b219abf17472f2e28ac3020f4560fb688
SavvySBrar <112981417+SavvySBrar@users.noreply.github.com>
2022-10-20 16:32:17 -0600

    compare files to instructor version

---
+++ demo-toggle-nav-start/js/main.js
@@ -0,0 +1,6 @@
+// Step 1: Defining Constants
+
+// Step 2: Adding A Click Event
+document.querySelector('.toggle-btn').addEventListener('click', () => {    
+    document.querySelector('.menu').classList.toggle('shazam');
+  }); 
